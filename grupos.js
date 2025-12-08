1806e03d0941a7521521698b974f17ef6398a488
Edson Faria <far3d@outlook.com>
2025-12-08 19:42:58 -0300

    Novas páginas

---
+++ grupos.js
@@ -0,0 +1,310 @@
+const grupos = [
+
+    {
+        id: "A1",
+        selecao: "México",
+        bandeira: "mexico.png",
+        alt: "Bandeira México",
+    },
+    {
+        id: "A2",
+        selecao: "África do Sul",
+        bandeira: "africadosul.png",
+        alt: "Bandeira África do Sul",
+    },
+    {
+        id: "A3",
+        selecao: "Coreia do Sul",
+        bandeira: "coreiadosul.png",
+        alt: "Bandeira Coreia do Sul",
+    },
+    {
+        id: "A4",
+        selecao: "República Tcheca/Irlanda/Dinamarca/Macedônia do Norte",
+        bandeira: "onu.png",
+        alt: "Bandeira República Tcheca/Irlanda/Dinamarca/Macedônia do Norte",
+    },
+    {
+        id: "B1",
+        selecao: "Canadá",
+        bandeira: "canada.png",
+        alt: "Bandeira Canadá",
+    },
+    {
+        id: "B2",
+        selecao: "Itália/Irlanda do Norte/País de Gales/Bósnia",
+        bandeira: "onu.png",
+        alt: "Bandeira Itália/Irlanda do Norte/País de Gales/Bósnia",
+    },
+    {
+        id: "B3",
+        selecao: "Catar",
+        bandeira: "catar.png",
+        alt: "Bandeira Catar",
+    },
+    {
+        id: "B4",
+        selecao: "Suíça",
+        bandeira: "suica.png",
+        alt: "Bandeira Suíça",
+    },
+    {
+        id: "C1",
+        selecao: "Brasil",
+        bandeira: "brasil.png",
+        alt: "Bandeira Brasil",
+    },
+    {
+        id: "C2",
+        selecao: "Marrocos",
+        bandeira: "marrocos.png",
+        alt: "Bandeira Marrocos",
+    },
+    {
+        id: "C3",
+        selecao: "Haiti",
+        bandeira: "haiti.png",
+        alt: "Bandeira Haiti",
+    },
+    {
+        id: "C4",
+        selecao: "Escócia",
+        bandeira: "escocia.png",
+        alt: "Bandeira Escócia",
+    },
+    {
+        id: "D1",
+        selecao: "Estados Unidos",
+        bandeira: "estadosunidos.png",
+        alt: "Bandeira Estados Unidos",
+    },
+    {
+        id: "D2",
+        selecao: "Paraguai",
+        bandeira: "paraguai.png",
+        alt: "Bandeira Paraguai",
+    },
+    {
+        id: "D3",
+        selecao: "Austrália",
+        bandeira: "australia.png",
+        alt: "Bandeira Austrália",
+    },
+    {
+        id: "D4",
+        selecao: "Turquia/Romênia/Eslováquia/Kosovo",
+        bandeira: "onu.png",
+        alt: "Bandeira Turquia/Romênia/Eslováquia/Kosovo",
+    },
+    {
+        id: "E1",
+        selecao: "Alemanha",
+        bandeira: "alemanha.png",
+        alt: "Bandeira Alemanha",
+    },
+    {
+        id: "E2",
+        selecao: "Curaçao",
+        bandeira: "curacao.png",
+        alt: "Bandeira Curaçao",
+    },
+    {
+        id: "E3",
+        selecao: "Costa do Marfim",
+        bandeira: "costadomarfim.png",
+        alt: "Bandeira Costa do Marfim",
+    },
+    {
+        id: "E4",
+        selecao: "Equador",
+        bandeira: "equador.png",
+        alt: "Bandeira Equador",
+    },
+    {
+        id: "F1",
+        selecao: "Holanda",
+        bandeira: "holanda.png",
+        alt: "Bandeira Holanda",
+    },
+    {
+        id: "F2",
+        selecao: "Japão",
+        bandeira: "japao.png",
+        alt: "Bandeira Japão",
+    },
+    {
+        id: "F3",
+        selecao: "Ucrânia/Suécia/Polônia/Albânia",
+        bandeira: "onu.png",
+        alt: "Bandeira Ucrânia/Suécia/Polônia/Albânia",
+    },
+    {
+        id: "F4",
+        selecao: "Tunísia",
+        bandeira: "tunisia.png",
+        alt: "Bandeira Tunísia",
+    },
+    {
+        id: "G1",
+        selecao: "Bélgica",
+        bandeira: "belgica.png",
+        alt: "Bandeira Bélgica",
+    },
+    {
+        id: "G2",
+        selecao: "Egito",
+        bandeira: "egito.png",
+        alt: "Bandeira Egito",
+    },
+    {
+        id: "G3",
+        selecao: "Irã",
+        bandeira: "ira.png",
+        alt: "Bandeira Irã",
+    },
+    {
+        id: "G4",
+        selecao: "Nova Zelândia",
+        bandeira: "novazelandia.png",
+        alt: "Bandeira Nova Zelândia",
+    },
+    {
+        id: "H1",
+        selecao: "Espanha",
+        bandeira: "espanha.png",
+        alt: "Bandeira Espanha",
+    },
+    {
+        id: "H2",
+        selecao: "Cabo Verde",
+        bandeira: "caboverde.png",
+        alt: "Bandeira Cabo Verde",
+    },
+    {
+        id: "H3",
+        selecao: "Arábia Saudita",
+        bandeira: "arabiasaudita.png",
+        alt: "Bandeira Arábia Saudita",
+    },
+    {
+        id: "H4",
+        selecao: "Uruguai",
+        bandeira: "uruguai.png",
+        alt: "Bandeira Uruguai",
+    },
+    {
+        id: "I1",
+        selecao: "França",
+        bandeira: "franca.png",
+        alt: "Bandeira França",
+    },
+    {
+        id: "I2",
+        selecao: "Senegal",
+        bandeira: "senegal.png",
+        alt: "Bandeira Senegal",
+    },
+    {
+        id: "I3",
+        selecao: "Bolívia/Suriname/Iraque",
+        bandeira: "onu.png",
+        alt: "Bandeira Bolívia/Suriname/Iraque",
+    },
+    {
+        id: "I4",
+        selecao: "Noruega",
+        bandeira: "noruega.png",
+        alt: "Bandeira Noruega",
+    },
+    {
+        id: "J1",
+        selecao: "Argentina",
+        bandeira: "argentina.png",
+        alt: "Bandeira Argentina",
+    },
+    {
+        id: "J2",
+        selecao: "Argélia",
+        bandeira: "argelia.png",
+        alt: "Bandeira Argélia",
+    },
+    {
+        id: "J3",
+        selecao: "Áustria",
+        bandeira: "austria.png",
+        alt: "Bandeira Áustria",
+    },
+    {
+        id: "J4",
+        selecao: "Jordânia",
+        bandeira: "jordania.png",
+        alt: "Bandeira Jordânia",
+    },
+    {
+        id: "K1",
+        selecao: "Portugal",
+        bandeira: "portugal.png",
+        alt: "Bandeira Portugal",
+    },
+    {
+        id: "K2",
+        selecao: "Congo/Jamaica/Nova Caledônia",
+        bandeira: "onu.png",
+        alt: "Bandeira Congo/Jamaica/Nova Caledônia",
+    },
+    {
+        id: "K3",
+        selecao: "Uzbequistão",
+        bandeira: "uzbequistao.png",
+        alt: "Bandeira Uzbequistão",
+    },
+    {
+        id: "K4",
+        selecao: "Colômbia",
+        bandeira: "colombia.png",
+        alt: "Bandeira Colômbia",
+    },
+    {
+        id: "L1",
+        selecao: "Inglaterra",
+        bandeira: "inglaterra.png",
+        alt: "Bandeira Inglaterra",
+    },
+    {
+        id: "L2",
+        selecao: "Croácia",
+        bandeira: "croacia.png",
+        alt: "Bandeira Croácia",
+    },
+    {
+        id: "L3",
+        selecao: "Gana",
+        bandeira: "gana.png",
+        alt: "Bandeira Gana",
+    },
+    {
+        id: "L4",
+        selecao: "Panamá",
+        bandeira: "panama.png",
+        alt: "Bandeira Panamá",
+    }
+
+];
+
+
+// GRUPOS
+grupos.forEach((pais) => {
+  const elemento = document.getElementById(pais.id);
+
+  // Altera o texto
+  const nomeSpan = elemento.querySelector("span");
+  nomeSpan.textContent = pais.selecao;
+
+  // Altera a imagem
+  const img = elemento.querySelector("img");
+  img.src = `bandeiras/${pais.bandeira}`;
+  img.alt = pais.alt;
+
+});
+// FIM Grupos
+
